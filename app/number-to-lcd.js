b28ed021d3a003f14003fcff840094001d647b7d
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-04-15 09:59:00 +0200

    Division of digit into lines

--- app/number-to-lcd.js
+++ app/number-to-lcd.js
@@ -1,4 +1,4 @@
-const singleNumbers = [
+const SINGLE_NUMBERS = [
     `
 ###
 # #
@@ -26,7 +26,7 @@ const singleNumbers = [
 ###
   #
 ###
-`,`
+`, `
 # #
 # #
 ###
@@ -70,9 +70,93 @@ const singleNumbers = [
 `]
 
 function numberToLcd(number) {
-    return singleNumbers[number] || singleNumbers[1]
+    return SINGLE_NUMBERS[number] || SINGLE_NUMBERS[1]
+}
+
+const XXX = '###'
+const X_X = '# #'
+const __X = '  #'
+const X__ = '#  '
+const _X_ = ' # '
+
+const DIGIT_LINES = [
+    [
+        XXX,
+        X_X,
+        X_X,
+        X_X,
+        XXX
+    ],
+    [
+        __X,
+        __X,
+        __X,
+        __X,
+        __X,
+    ],
+    [
+        XXX,
+        __X,
+        XXX,
+        X__,
+        XXX,
+    ],
+    [
+        XXX,
+        __X,
+        XXX,
+        __X,
+        XXX,
+    ],
+    [
+        X_X,
+        X_X,
+        XXX,
+        __X,
+        __X,
+    ],
+    [
+        XXX,
+        X__,
+        XXX,
+        __X,
+        XXX,
+    ],
+    [
+        XXX,
+        X__,
+        XXX,
+        X_X,
+        XXX,
+    ],
+    [
+        XXX,
+        __X,
+        _X_,
+        _X_,
+        _X_,
+    ],
+    [
+        XXX,
+        X_X,
+        XXX,
+        X_X,
+        XXX,
+    ],
+    [
+        XXX,
+        X_X,
+        XXX,
+        __X,
+        XXX,
+    ]
+]
+
+function digitLine(digit, line) {
+    return DIGIT_LINES[digit][line - 1]
 }
 
 module.exports = {
-    numberToLcd
+    numberToLcd,
+    digitLine
 }
--- test/lcd-numbers.js
+++ test/lcd-numbers.js
@@ -1,105 +1,172 @@
 require('chai').should()
 
-const {numberToLcd} = require('../app/number-to-lcd')
+const {numberToLcd, digitLine} = require('../app/number-to-lcd')
 
 describe('Number to LCD', () => {
-    it('converts 0', () => {
-        numberToLcd(0).should.equal(`
+    context('Single digits', function () {
+        it('converts 0', () => {
+            numberToLcd(0).should.equal(`
 ###
 # #
 # #
 # #
 ###
 `)
-    })
+        })
 
-    it('converts 1', () => {
-        numberToLcd(1).should.equal(`
+        it('converts 1', () => {
+            numberToLcd(1).should.equal(`
   #
   #
   #
   #
   #
 `)
-    })
+        })
 
-    it('converts 2', () => {
-        numberToLcd(2).should.equal(`
+        it('converts 2', () => {
+            numberToLcd(2).should.equal(`
 ###
   #
 ###
 #  
 ###
 `)
-    })
+        })
 
-    it('converts 3', () => {
-        numberToLcd(3).should.equal(`
+        it('converts 3', () => {
+            numberToLcd(3).should.equal(`
 ###
   #
 ###
   #
 ###
 `)
-    })
+        })
 
-    it('converts 4', () => {
-        numberToLcd(4).should.equal(`
+        it('converts 4', () => {
+            numberToLcd(4).should.equal(`
 # #
 # #
 ###
   #
   #
 `)
-    })
+        })
 
-    it('converts 5', () => {
-        numberToLcd(5).should.equal(`
+        it('converts 5', () => {
+            numberToLcd(5).should.equal(`
 ###
 #  
 ###
   #
 ###
 `)
-    })
+        })
 
-    it('converts 6', () => {
-        numberToLcd(6).should.equal(`
+        it('converts 6', () => {
+            numberToLcd(6).should.equal(`
 ###
 #  
 ###
 # #
 ###
 `)
-    })
+        })
 
-    it('converts 7', () => {
-        numberToLcd(7).should.equal(`
+        it('converts 7', () => {
+            numberToLcd(7).should.equal(`
 ###
   #
  # 
  # 
  # 
 `)
-    })
+        })
 
-    it('converts 8', () => {
-        numberToLcd(8).should.equal(`
+        it('converts 8', () => {
+            numberToLcd(8).should.equal(`
 ###
 # #
 ###
 # #
 ###
 `)
-    })
+        })
 
-    it('converts 9', () => {
-        numberToLcd(9).should.equal(`
+        it('converts 9', () => {
+            numberToLcd(9).should.equal(`
 ###
 # #
 ###
   #
 ###
 `)
+        })
+    })
+
+    context('Two digits', () => {
+        xit('converts 10', () => {
+            numberToLcd(10).should.equal(`
+  # ###
+  # # #
+  # # #
+  # # #
+  # ###
+`)
+        })
+    })
+
+    describe('Digit division in lines', () => {
+        context('For the digit "0"', () => {
+            it('returns "###" as the first line', () => {
+                digitLine(0, 1).should.equal('###')
+            })
+
+            it('returns "# #" as the second line', () => {
+                digitLine(0, 2).should.equal('# #')
+            })
+
+            it('returns "# #" as the third line', () => {
+                digitLine(0, 3).should.equal('# #')
+            })
+
+            it('returns "###" as the last line', () => {
+                digitLine(0, 5).should.equal('###')
+            })
+        })
+
+        context('For the digit "1"', () => {
+            it('returns " #" as the first line', () => {
+                digitLine(1, 1).should.equal('  #')
+            })
+        })
+
+        context('For the other digits', () => {
+            it('returns "###" as the first line for 2', () => {
+                digitLine(2, 1).should.equal('###')
+            })
+
+            it('returns "#  " as the second line for 2', () => {
+                digitLine(2, 2).should.equal('  #')
+            })
+
+            it('returns "#  " as the fourth line for 2', () => {
+                digitLine(2, 4).should.equal('#  ')
+            })
+
+            it('returns "###" as the first line for 3', () => {
+                digitLine(3, 1).should.equal('###')
+            })
+
+            it('returns "# #" as the first line for 4', () => {
+                digitLine(4, 1).should.equal('# #')
+            })
+
+            it('returns "###" as the first line for 5', () => {
+                digitLine(5, 1).should.equal('###')
+            })
+        })
     })
 })
+
