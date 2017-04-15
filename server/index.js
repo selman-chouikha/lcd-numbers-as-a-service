dc5b9c3cf3e59fa7d826c06e22b5def6284f516c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-04-15 19:17:40 +0200

    Service ok

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const Hapi = require('hapi')
 
 const config = require('config')
 
-// const {numberToLcd} = require('../app/number-to-lcd')
+const {numberToLcd} = require('../app/number-to-lcd')
 
 const server = new Hapi.Server()
 
@@ -18,20 +18,21 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function(request, reply) {
-        const exampleUrl = `${baseUrl}/12345`
+        const exampleUrl = `${baseUrl}/lcd_numbers/12345`
         reply(`Please use the "/{number}" route.<br/>Example: <a href="${exampleUrl}">${exampleUrl}</a>`)
     }
 })
 
-// server.route({
-//     method: 'GET',
-//     path: '/{name}',
-//     handler: function(request, reply) {
-//         const numberAsString = encodeURIComponent(request.params.name)
-//         const number = parseInt(numberAsString)
-//         reply({number: number, numberAsLcdString: numberToLcd(number)})
-//     }
-// })
+server.route({
+    method: 'GET',
+    path: '/lcd_numbers/{number}',
+    handler: function(request, reply) {
+        const numberAsString = encodeURIComponent(request.params.number)
+        const number = parseInt(numberAsString)
+        reply(numberToLcd(number))
+            .header('Content-Type', 'text/plain; charset=utf-8')
+    }
+})
 
 server.start((err) => {
     if (err) {
--- test/end-to-end/lcd-numbers-as-a-service.js
+++ test/end-to-end/lcd-numbers-as-a-service.js
@@ -2,14 +2,14 @@ const request = require('supertest')
 const config = require('config')
 
 describe('End to end tests', () => {
-    context('The home page', () => {
-        beforeEach(() => {
-            const protocol = config.get('server.protocol')
-            const hostname = config.get('server.hostname')
-            const port = config.get('server.port')
-            this.baseUrl = `${protocol}://${hostname}:${port}`
-        })
+    beforeEach(() => {
+        const protocol = config.get('server.protocol')
+        const hostname = config.get('server.hostname')
+        const port = config.get('server.port')
+        this.baseUrl = `${protocol}://${hostname}:${port}`
+    })
 
+    context('The home page', () => {
         it('responds', (done) => {
             request(this.baseUrl)
                 .get('/')
@@ -22,6 +22,29 @@ describe('End to end tests', () => {
             request(this.baseUrl)
                 .get('/')
                 .expect(new RegExp(this.baseUrl))
+                .end(done)
+        })
+    })
+
+    context('The lcd number API', () => {
+        it('responds 200 with plain text', (done) => {
+            request(this.baseUrl)
+                .get('/lcd_numbers/1')
+                .expect(200)
+                .expect('Content-Type', 'text/plain; charset=utf-8')
+                .end(done)
+        })
+
+        it('returns 1 LCD representation in body', (done) => {
+            request(this.baseUrl)
+                .get('/lcd_numbers/1')
+                .expect(`
+  #
+  #
+  #
+  #
+  #
+`)
                 .end(done)
         })
     })
